cb2225a8a86b2e66ac9761df088f8a2726639df9
Connor Feucht <132318582+connorkfeucht@users.noreply.github.com>
2023-06-12 13:36:39 -0600

    Removed Crouching

---
+++ Assets/CrouchScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrouchScript : MonoBehaviour
+{
+    private Vector2 normalHeight;
+    private Vector2 crouchHeight;
+
+    void Start()
+    {
+        normalHeight = transform.localScale;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,12 +10,6 @@ public class PlayerMovement : MonoBehaviour
     private bool isFacingRight = true;
     private Animator animator;
 
-    public SpriteRenderer spriteRenderer;
-    public Sprite standingSprite;
-    public Sprite crouchingSprite;
-    public BoxCollider2D b;
-    public Vector2 standingSize;
-    public Vector2 crouchingSize;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -23,30 +17,10 @@ public class PlayerMovement : MonoBehaviour
 
     void Start() {
         animator = GetComponent<Animator>();
-
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.sprite = standingSprite;
-        standingSize = b.size;
     }
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Debug.Log("Crouch");
-            spriteRenderer.sprite = crouchingSprite;
-            b.size = crouchingSize;
-            b.offset = new Vector2(0f, -0.5f);
-            
-        }
-        else if (Input.GetButtonUp("Fire1"))
-        {
-            Debug.Log("Stand");
-            spriteRenderer.sprite = standingSprite;
-            b.size = standingSize;
-            b.offset = new Vector2(0f, 0f);
-        }
 
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
