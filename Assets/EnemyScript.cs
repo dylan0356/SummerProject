73bd68c7cf77a27f75760a808caf8e4c8e75ea27
Connor Feucht <132318582+connorkfeucht@users.noreply.github.com>
2023-06-15 14:35:53 -0600

    Fixed Enemy not following player

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -55,6 +55,7 @@ public class EnemyScript : MonoBehaviour
 
         if (hit.collider != null) {
             if (hit.collider.gameObject.CompareTag("Player")) {
+                Debug.Log("see plaayer");
                 val = true;
             } else {
                 val = false;
