a42cba26e8bcbf5f9df06d0e8e9fac7ce179d028
Connor Feucht <132318582+connorkfeucht@users.noreply.github.com>
2023-07-04 11:57:08 -0600

    Implemented player attack animation

--- Assets/PlayerAttack.cs
+++ Assets/PlayerAttack.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     private GameObject attackArea = default;
+    private Animator animator;
 
     private bool attacking = false;
+    
 
     private float timeToAttack = 0.25f;
     private float timer = 0f;
@@ -14,16 +16,20 @@ public class PlayerAttack : MonoBehaviour
     void Start()
     {
         attackArea = transform.GetChild(0).gameObject;
+        animator = GetComponent<Animator>();
     }
 
     
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0)) {
-            Debug.Log("Attack");
             Attack();
         }
-
+        if (attacking) {
+            animator.SetBool("attacking", true);
+        } else {
+            animator.SetBool("attacking", false);
+        }
         if (attacking) {
             timer += Time.deltaTime;
             if (timer >= timeToAttack) {
@@ -35,6 +41,7 @@ public class PlayerAttack : MonoBehaviour
         }
     }
 
+    
     private void Attack() {
         attacking = true;
         attackArea.SetActive(true);
