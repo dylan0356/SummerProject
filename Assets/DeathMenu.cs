a740867ce5b6b53c53ac124b0a086aa19a94cabe
Connor Feucht <132318582+connorkfeucht@users.noreply.github.com>
2023-07-18 20:54:43 -0600

    Made buttons work on death menu

--- Assets/DeathMenu.cs
+++ Assets/DeathMenu.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void PlayGame() {
+        // ScoreScript.ResetScore();
+        // PlayerHealth.health = 5;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void QuitGame() {
+        Application.Quit();
     }
 }
