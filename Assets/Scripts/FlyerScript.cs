8acd4ee774c91fd2fe1b3529cd88fbcf5f95fb55
Connor Feucht <132318582+connorkfeucht@users.noreply.github.com>
2023-07-24 11:31:13 -0600

    Changed UI font

--- Assets/Scripts/FlyerScript.cs
+++ Assets/Scripts/FlyerScript.cs
@@ -10,8 +10,6 @@ public class FlyerScript : MonoBehaviour
     private float moveSpeed = 4f;
     private float engageDistance = 10f;
 
-    // private float duration = 0.5f;
-
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -30,22 +28,9 @@ public class FlyerScript : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.CompareTag("Player")) {
             PlayerHealth.health--;
-            // StartCoroutine(NoBox());
             if (PlayerHealth.health <= 0) {
                 collision.gameObject.GetComponent<PlayerHealth>().Death();
             }
         }
     }
-
-    // IEnumerator NoBox() {
-    //     float elapsed = 0f;
-    //     BoxCollider2D bc = GetComponent<BoxCollider2D>();
-    //     while (elapsed < duration) {
-    //         elapsed += Time.deltaTime;
-    //         bc.enabled = false;
-    //     }
-    //     bc.enabled = true;
-    //     return null;
-    // }
-
 }
