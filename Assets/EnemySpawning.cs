5a3005649ad798d707adda8cb64ebff24ef1c16d
Connor Feucht <132318582+connorkfeucht@users.noreply.github.com>
2023-07-26 17:23:59 -0600

    Enemy Spawning Scaling added
    more enemies spawn the higher your score gets

--- Assets/EnemySpawning.cs
+++ Assets/EnemySpawning.cs
@@ -7,17 +7,28 @@ public class EnemySpawning : MonoBehaviour
 
     [SerializeField] GameObject flyerEnemy;
 
-    private float flyerInterval = 2.5f;
+    private float flyerInterval = 10f;
+    private int lastScore;
+    private int difficultyScaler = 30;
 
     void Start()
     {
         StartCoroutine(spawnEnemy(flyerInterval, flyerEnemy));
+        lastScore = 0;
     }
+
 
     private IEnumerator spawnEnemy(float interval, GameObject enemy) {
         yield return new WaitForSeconds(interval);
         Debug.Log("Spawned enemy");
         GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0), Quaternion.identity);
+        if (interval != 1) {
+            if (ScoreScript.scoreVal >= lastScore + difficultyScaler) {
+            interval--;
+            lastScore = ScoreScript.scoreVal;
+            }
+        }
+
         StartCoroutine(spawnEnemy(interval, enemy));
     }
     
