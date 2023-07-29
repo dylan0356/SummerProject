b147014324419973ac88ffd12c824fcb4e62706a
Connor Feucht <132318582+connorkfeucht@users.noreply.github.com>
2023-07-29 16:01:19 -0600

    added start and quit sounds to main menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,12 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    [SerializeField] private AudioSource startSound;
+    [SerializeField] private AudioSource quitSound;
+
     public void PlayGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        startSound.Play();
     }
 
     public void QuitGame() {
         Application.Quit();
+        quitSound.Play();
     }
 
 }
